c75e3a3dd1b4ea1a932859c5b1de431e812258f2
Simon Josef Heilmeier <simon.heilmeier@hotmail.de>
2019-01-22 19:11:33 +0100

    Anmelde und Registrierungs Dialog hinzugefügt

--- app/src/main/java/de/javaalberto/berufsschulefreising/pampelv2/MainActivity.java
+++ app/src/main/java/de/javaalberto/berufsschulefreising/pampelv2/MainActivity.java
@@ -1,9 +1,11 @@
 package de.javaalberto.berufsschulefreising.pampelv2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,20 +22,27 @@ import java.security.cert.X509Certificate;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvRueckgabe,tvLimit;
+    private TextView tvRueckgabe,tvLimit,tvNote;
     private ImageView ivSmiley;
-
+    private boolean isAuthenticated;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Utils neueUtils = new Utils(this);
-
         setContentView(R.layout.activity_main);
+        isAuthenticated = false;
+
+        if (!isAuthenticated) {
+            showSignUp();
+        }
+
+        final Utils neueUtils = new Utils(this);
         tvRueckgabe = findViewById(R.id.tvRueckgabe);
         tvLimit = findViewById(R.id.tvLimit);
+        tvNote = findViewById(R.id.tvNote);
         ivSmiley = findViewById(R.id.ivSmiley);
         disableSSLCertificateChecking();
 
@@ -51,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                         tvRueckgabe.setText(String.format("%d km/h", value));
                         setLimitIcon(l_Limit);
                         setSpeedIcon(value,l_Limit);
+                        setHighscore(100,90);
                     }
             );
         });
@@ -76,6 +86,109 @@ public class MainActivity extends AppCompatActivity {
                 return 0;
             }
         }
+    }
+
+    public void setHighscore(int high_highscore,int own_Highscore ) {
+        //
+        tvNote.setText(String.format("Highscore: %d \n Score: %d",high_highscore,own_Highscore));
+    }
+
+    public void setLimitIcon(int i_Limit) {
+        if (i_Limit > 0) {
+            tvLimit.setVisibility(View.VISIBLE);
+            if (i_Limit > 200) {
+                tvLimit.setText("");
+                tvLimit.setBackgroundResource(R.drawable.nospeedsign);
+            } else {
+                tvLimit.setBackgroundResource(R.drawable.emptysign);
+                tvLimit.setText(String.format("%d", i_Limit));
+            }
+        } else {
+            tvLimit.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void setSpeedIcon(int i_Speed,int i_Limit) {
+
+        if (i_Speed == 0) {
+            tvLimit.setVisibility(View.INVISIBLE);
+            tvRueckgabe.setVisibility(View.INVISIBLE);
+            tvNote.setVisibility(View.VISIBLE);
+        } else {
+            tvLimit.setVisibility(View.VISIBLE);
+            tvRueckgabe.setVisibility(View.VISIBLE);
+            tvNote.setVisibility(View.INVISIBLE);
+        }
+
+        if ((i_Speed != 69) && (i_Speed != 0) ){
+            int l_istToFast = isToFast(i_Speed, i_Limit);
+            if (l_istToFast == 100) {
+                ivSmiley.setImageResource(R.drawable.smileybad);
+            } else if (l_istToFast == 10) {
+                ivSmiley.setImageResource(R.drawable.smileysad);
+            } else if (l_istToFast == 0) {
+                ivSmiley.setImageResource(R.drawable.smileyhappy);
+            }
+        } else if (i_Speed == 0) {
+            ivSmiley.setImageResource(R.drawable.smileysleepy);
+        } else if (i_Speed == 69) {
+            ivSmiley.setImageResource(R.drawable.smileywink);
+        } else {
+            ivSmiley.setImageResource(R.drawable.smileyok);
+        }
+    }
+
+    public void showSignUp() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+        final EditText mName = mView.findViewById(R.id.edtName);
+        final EditText mPass = mView.findViewById(R.id.edtPass);
+        mBuilder.setPositiveButton(R.string.login, (dialog, id) -> {
+            if (!mName.getText().toString().isEmpty() && !mPass.getText().toString().isEmpty()) {
+                //Todo RODI richtige anmelden einbinden
+                //if loginUtils.anmelden(mName.getText().toString(), mPass.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, getString(R.string.login_success), Toast.LENGTH_SHORT).show();
+                //} else {
+                //    Toast.makeText(MainActivity.this, getString(R.string.login_fail), Toast.LENGTH_SHORT).show();
+                //}
+            } else {
+                Toast.makeText(MainActivity.this, getString(R.string.login_fail), Toast.LENGTH_SHORT).show();
+                showSignUp();
+            }
+        });
+        mBuilder.setNeutralButton(R.string.regist, (dialog, id) -> {
+            showRegistration();
+        });
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+    }
+
+    private void showRegistration() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_registration, null);
+        final EditText mName = mView.findViewById(R.id.edtName);
+        final EditText mPass = mView.findViewById(R.id.edtPass);
+        final EditText mPassConf = mView.findViewById(R.id.edtPassConf);
+
+        mBuilder.setPositiveButton(R.string.login, (dialog, id) -> {
+            if (!mName.getText().toString().isEmpty()
+                    && !mPass.getText().toString().isEmpty()
+                    && mPass.getText().toString().equals(mPassConf.getText().toString())) {
+                //Todo RODI richtige register einbinden
+                //if loginUtils.registrieren(mName.getText().toString(), mPass.getText().toString().isEmpty()) {
+                Toast.makeText(MainActivity.this, getString(R.string.regist_success), Toast.LENGTH_SHORT).show();
+                //} else {
+                //    Toast.makeText(MainActivity.this, getString(R.string.login_fail), Toast.LENGTH_SHORT).show();
+                //}
+            } else {
+                Toast.makeText(MainActivity.this, getString(R.string.registFailed), Toast.LENGTH_SHORT).show();
+                showRegistration();
+            }
+        });
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
     }
 
     private static void disableSSLCertificateChecking() {
@@ -105,40 +218,6 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void setLimitIcon(int i_Limit) {
-        if (i_Limit > 0) {
-            tvLimit.setVisibility(View.VISIBLE);
-            if (i_Limit > 200) {
-                tvLimit.setText("");
-                tvLimit.setBackgroundResource(R.drawable.nospeedsign);
-            } else {
-                tvLimit.setBackgroundResource(R.drawable.emptysign);
-                tvLimit.setText(String.format("%d", i_Limit));
-            }
-        } else {
-            tvLimit.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    public void setSpeedIcon(int i_Speed,int i_Limit) {
-        if ((i_Speed != 69) && (i_Speed != 0) ){
-            int l_istToFast = isToFast(i_Speed, i_Limit);
-            if (l_istToFast == 100) {
-                ivSmiley.setImageResource(R.drawable.smileybad);
-            } else if (l_istToFast == 10) {
-                ivSmiley.setImageResource(R.drawable.smileysad);
-            } else if (l_istToFast == 0) {
-                ivSmiley.setImageResource(R.drawable.smileyhappy);
-            }
-        } else if (i_Speed == 0) {
-            ivSmiley.setImageResource(R.drawable.smileysleepy);
-        } else if (i_Speed == 69) {
-            ivSmiley.setImageResource(R.drawable.smileywink);
-        } else {
-            ivSmiley.setImageResource(R.drawable.smileyok);
         }
     }
 }
