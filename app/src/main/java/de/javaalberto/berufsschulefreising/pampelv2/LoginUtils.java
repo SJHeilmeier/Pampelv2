f97a6033c293268ba45b6b0f56abea9df1e74d11
Brotmeister <roderich.obenaus@web.de>
2019-01-23 20:00:09 +0100

    fertige handleScore Funktion

--- app/src/main/java/de/javaalberto/berufsschulefreising/pampelv2/LoginUtils.java
+++ app/src/main/java/de/javaalberto/berufsschulefreising/pampelv2/LoginUtils.java
@@ -14,6 +14,85 @@ public class LoginUtils {
 
     private String name;
     private Integer id;
+    private Integer score;
+    private String firstName;
+    private String secondName;
+    private String thirdName;
+    private Integer firstScore;
+    private Integer secondScore;
+    private Integer thirdScore;
+
+    public Integer handleScore(String username, Integer currSpeed, Integer currLimit) {
+        try {
+            String scoreUrl = String.format(
+                    Locale.ROOT,
+                    "https://pampelv2.dev-juicy-roots.com/api/users/score.php?username=%s&currSpeed=%d&currLimit=%d",
+                    username,
+                    currSpeed,
+                    currLimit
+
+            );
+
+            HttpURLConnection connScore = (HttpURLConnection) new URL(scoreUrl).openConnection();
+            BufferedReader readerScore = null;
+
+            readerScore = new BufferedReader(new InputStreamReader(connScore.getInputStream()));
+
+            StringBuilder sbScore = new StringBuilder();
+            String lineScore;
+
+            while ((lineScore = readerScore.readLine()) != null) {
+                sbScore.append(lineScore);
+            }
+
+            readerScore.close();
+            connScore.disconnect();
+
+            JSONObject jsonObjectScore = new JSONObject(sbScore.toString());
+
+            final String loginMessage = jsonObjectScore
+                    .getString("message");
+
+            final String loginStatus = jsonObjectScore
+                    .getString("status");
+
+            name = jsonObjectScore
+                    .getString("username");
+
+            score = jsonObjectScore
+                    .getInt("score");
+
+            //erster Platz
+            firstName = jsonObjectScore
+                    .getJSONObject("first").getString("username");
+            firstScore = jsonObjectScore
+                    .getJSONObject("first").getInt("score");
+
+            //zweiter Platz
+            secondName = jsonObjectScore
+                    .getJSONObject("second").getString("username");
+            secondScore = jsonObjectScore
+                    .getJSONObject("second").getInt("score");
+
+            //dritter Platz
+            thirdName = jsonObjectScore
+                    .getJSONObject("third").getString("username");
+            thirdScore = jsonObjectScore
+                    .getJSONObject("third").getInt("score");
+
+
+            if (loginStatus.equals("true")) {
+                return score;
+            } else return -1;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
 
     public boolean anmelden(String username, String password) {
         try {
@@ -130,7 +209,31 @@ public class LoginUtils {
         return name;
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getScore() {
+        return score;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getThirdName() {
+        return thirdName;
+    }
+
+    public Integer getFirstScore() {
+        return firstScore;
+    }
+
+    public Integer getSecondScore() {
+        return secondScore;
+    }
+
+    public Integer getThirdScore() {
+        return thirdScore;
     }
 }
